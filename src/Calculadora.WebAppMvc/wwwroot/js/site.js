db5db6946f6c0c6945b752163a53c637e33b1353
unknown <elvislimap@gmail.com>
2020-02-21 10:09:10 -0300

    Subindo projeto

---
+++ src/Calculadora.WebAppMvc/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    var agregado = 0;
+
+    $(".btn-numero").click(function () {
+        let valorVisor = $("#visorCalculadora").val();
+        valorVisor += $(this).html();
+
+        $("#visorCalculadora").val(valorVisor);
+    });
+
+    $(".btn-operacao").click(function () {
+        let operacao = $(this).html();
+        let valorVisor = parseInt($("#visorCalculadora").val());
+
+        switch (operacao) {
+            case "+":
+                agregado = agregado == 0 ? valorVisor : agregado + valorVisor;
+                $("#visorCalculadora").val("");
+                break;
+            case "-":
+                break;
+            case "x":
+                break;
+            case "/":
+                break;
+
+            default:
+                $("#visorCalculadora").val(agregado);
+                agregado = 0;
+                break;
+        }
+    });
+});
